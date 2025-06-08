e96cb401de981a3ce9ec56746ea39b528e93c420
Akio Ckist <quan.tran230220@vnuk.edu.vn>
2025-06-08 13:05:00 +0700

    feat: restructure authentication logic by moving auth.js to lib and updating import paths

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,2 +1,2 @@
-import { handlers } from "../../../../../auth" // Adjust path based on your project structure
+import { handlers } from "./../../../../../lib/auth" // Adjust path based on your project structure
 export const { GET, POST } = handlers
